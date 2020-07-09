792e7ce93fe111adeaffbefb3721351da5c105ff
NabeelahY <noblenab13@gmail.com>
2020-07-09 15:15:12 +0100

    Add validation to registration route

--- src/routes/user/register.ts
+++ src/routes/user/register.ts
@@ -1,16 +1,26 @@
 import { Router, Request, Response } from 'express';
 import { UserMethods } from '../../database/users/user.statics';
+import validate from '../../middlewares/validate';
+import { registerFields } from '../../middlewares/validateRegister';
 const router = Router();
 
-router.post('/register', async (req: Request, res: Response) => {
-  const { username, email, password } = req.body;
+router.post(
+  '/register',
+  validate(registerFields),
+  async (req: Request, res: Response) => {
+    try {
+      const { username, email, password } = req.body;
 
-  const { newUser, token } = await UserMethods.register({
-    params: { username, email, password },
-  });
+      const { newUser, token } = await UserMethods.register({
+        params: { username, email, password },
+      });
+      delete newUser.password;
 
-  res.status(201).json({ user: newUser, token });
-});
+      res.status(201).json({ user: newUser, token });
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+);
 
-
-export {router as registerRouter}
+export { router as registerRouter };
