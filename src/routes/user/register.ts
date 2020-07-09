b6ac206a0fee425961ae01e181bf65eb9db95694
NabeelahY <noblenab13@gmail.com>
2020-07-09 20:09:28 +0100

    Add more middlewares

--- src/routes/user/register.ts
+++ src/routes/user/register.ts
@@ -2,11 +2,15 @@ import { Router, Request, Response } from 'express';
 import { UserMethods } from '../../database/users/user.statics';
 import validate from '../../middlewares/validate';
 import { registerFields } from '../../middlewares/validateRegister';
+import { emailDoesNotExist } from '../../middlewares/email_not_exist';
+import { usernameDoesNotExist } from '../../middlewares/username_not_exist';
 const router = Router();
 
 router.post(
   '/register',
   validate(registerFields),
+  usernameDoesNotExist,
+  emailDoesNotExist,
   async (req: Request, res: Response) => {
     try {
       const { username, email, password } = req.body;
