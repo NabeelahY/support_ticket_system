ce80f744771c504ec64149d8ef65e83767719c4d
NabeelahY <noblenab13@gmail.com>
2020-07-10 21:09:30 +0100

    Change timeout

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -19,7 +19,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
-  jest.setTimeout(50000);
+  jest.setTimeout(30000);
 
   const collections = await mongoose.connection.db.collections();
 
