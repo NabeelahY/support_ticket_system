80edc7a5acb33f3adcb337379056a3ac42b3e014
NabeelahY <noblenab13@gmail.com>
2020-07-10 18:42:01 +0100

    Create tests for register route

---
+++ src/tests/index.ts
@@ -0,0 +1,32 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
+let mongo: any;
+
+beforeAll(async () => {
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  process.env.DATABASE_URL = mongoUri;
+
+  await mongoose.connect(process.env.DATABASE_URL!, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+});
+
+beforeEach(async () => {
+  jest.setTimeout(50000);
+
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
