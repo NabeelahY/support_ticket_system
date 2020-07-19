8b39f36236cc2adfa6b6d410f59b15a3efa8e25e
NabeelahY <noblenab13@gmail.com>
2020-07-19 21:43:18 +0100

    Create route for only admins to access all users tickets

---
+++ src/routes/support/getAllTickets.ts
@@ -0,0 +1,23 @@
+import { Router, Request, Response } from 'express';
+import { isAdmin } from '../../middlewares/isAdmin';
+import { restricted } from '../../middlewares/restricted';
+import { SupportMethods } from '../../database/support/support.statics';
+import { ticketDoesNotExist } from '../../middlewares/ticketDoesNotExist';
+
+const router = Router();
+
+router.get(
+  '/all/tickets',
+  restricted,
+  isAdmin,
+  async (req: Request, res: Response) => {
+    try {
+      const { tickets } = await SupportMethods.getAllTickets();
+      res.status(200).json(tickets);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+);
+
+export { router as getAllTicketRouter };
