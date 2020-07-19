9d07b613a9f4e808b79659a2178976fc418cae9d
NabeelahY <noblenab13@gmail.com>
2020-07-19 21:47:57 +0100

    Support agents and admin can get all tickets

--- src/routes/support/__tests__/getAllTickets.test.ts
+++ src/routes/support/__tests__/getAllTickets.test.ts
@@ -6,7 +6,7 @@ import { userDetail, supportUserDetail } from '../../../tests';
 const request = supertest(server);
 
 describe('[GET] Get All Tickets', () => {
-  it('Error if logged user is not an admin', async () => {
+  it('Error if logged user is not an admin or support agent', async () => {
     const { token } = await userDetail();
     const res = await request
       .get(`/api/support/all/tickets`)
@@ -15,7 +15,7 @@ describe('[GET] Get All Tickets', () => {
     expect(res.status).toEqual(401);
   });
 
-  it('Admin can get all tickets', async () => {
+  it('Admin and support agent can get all tickets', async () => {
     const { token } = await supportUserDetail();
     const res = await request
       .get(`/api/support/all/tickets`)
--- src/routes/support/getAllTickets.ts
+++ src/routes/support/getAllTickets.ts
@@ -2,14 +2,14 @@ import { Router, Request, Response } from 'express';
 import { isAdmin } from '../../middlewares/isAdmin';
 import { restricted } from '../../middlewares/restricted';
 import { SupportMethods } from '../../database/support/support.statics';
-import { ticketDoesNotExist } from '../../middlewares/ticketDoesNotExist';
+import { isSupportAgent } from '../../middlewares/isSupportAgent';
 
 const router = Router();
 
 router.get(
   '/all/tickets',
   restricted,
-  isAdmin,
+  isSupportAgent,
   async (req: Request, res: Response) => {
     try {
       const { tickets } = await SupportMethods.getAllTickets();
